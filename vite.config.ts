9b767278d106233b4e9f2b109b3fe8ea7f45fe63
dev-nain <hasnainahmadkhan04@gmail.com>
2026-07-24 18:19:36 +0500

    lint: fixed lint issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-	plugins: [tailwindcss(), reactRouter(), cloudflare({
-        viteEnvironment: {
-            name: "ssr"
-        }
-    })],
+	plugins: [
+		tailwindcss(),
+		reactRouter(),
+		cloudflare({
+			viteEnvironment: {
+				name: "ssr",
+			},
+		}),
+	],
 	resolve: {
 		tsconfigPaths: true,
 	},
